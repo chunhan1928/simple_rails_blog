373a8880eb0ac71d4c3526dec46985ec72195740
Chun-Han Chen <chunhanchen11235@gmail.com>
2024-11-18 01:20:49 +0800

    feat: create table tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_15_064926) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_17_090820) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -27,6 +27,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_15_064926) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
